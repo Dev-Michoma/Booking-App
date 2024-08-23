429b769b6603ed581a909441c86ca2feeb04069a
Dev <codesplint151@gmail.com>
2024-08-22 22:50:51 -0400

    configuring the login form

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 export  default function IndexPage(){
     return (
         <div>
@@ -8,7 +9,7 @@ export  default function IndexPage(){
      <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
      </svg>
     <span className="font-bold text-xl">
-      SafariStay
+      AfriSafari
     </span>
 
     </a>
@@ -25,7 +26,7 @@ export  default function IndexPage(){
       </button>
     
     </div>
-    <div className="flex gap-2 border-gray-300 rounded-full py-2 px-4 items-center shadow-md">
+    <Link  to ={"/login"} className="flex gap-2 border-gray-300 rounded-full py-2 px-4 items-center shadow-md">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
      </svg>
@@ -36,8 +37,7 @@ export  default function IndexPage(){
     </div>
 
 
-
-      </div>
+      </Link>
   </header>
 </div>
     );
