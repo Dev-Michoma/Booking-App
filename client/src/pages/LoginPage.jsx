59413c96edf1e84d0963aa2973e8134770983113
Dev <codesplint151@gmail.com>
2024-08-24 19:38:07 -0400

    centering of the login div

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,8 @@
 export default function LoginPage(){
     return (
-        <div className="mt-4 grow flex">
+        <div className="mt-4 grow flex items-center justify-around">
            
-            <div>
+            <div className="mb-32">
             <h1 className="text-4xl text-center mb-4">Login</h1>
             <form className="max-w-md mx-auto">
                 <input type="email" placeholder="youremail.com"/>
