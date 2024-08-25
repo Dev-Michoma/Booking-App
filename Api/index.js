dd05da3b5ad8d7106b0176b223506b604b0b1ee6
Dev <codesplint151@gmail.com>
2024-08-24 23:45:31 -0400

    Hashing of the password

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const User = require('./Models/User.js');
+const  bcrypt = require('bcryptjs');
+require('dotenv').config()
 const app = express();
+const  bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(cors({
@@ -10,14 +15,20 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 
 }));
-
+mongoose.connect(process.env.MONGO_URL);
+console.log(process.env.MONGO_URL);
 app.get ('/test' ,(req,res)=>{
     res.json('test ok');
 });
 
 app.post('/register' ,async (req,res) => {
    const {name ,email ,password} = req.body;
-   res.json({name ,email ,password});
-})
+   const userDoc  =  await User.create({
+    name,
+    email,
+    password:bcrypt.hashSync(password ,bcryptSalt),
+   });
+   res.json(userDoc);
+});
 
 app.listen(4000);
