f12b5979fa3bab9fb39875f141a24bbf536f2743
Dev <codesplint151@gmail.com>
2024-08-24 21:18:46 -0400

    Done connection from the Frontend to the Backend

--- Api/index.js
+++ Api/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(cors({
+   credentials: true,
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+
+}));
 
 app.get ('/test' ,(req,res)=>{
     res.json('test ok');
