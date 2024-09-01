c0f2620bb90dcd34fcee79de42c87fe5231beece
Dev <codesplint151@gmail.com>
2024-09-01 12:52:03 -0400

    code refactoring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import {Route ,Routes}  from "react-router-dom";
 import IndexPage  from './pages/IndexPage';
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage';
+import {useEffect } from "react"
 import Layout from './Layout';
 import axios from "axios";
 import {createContext} from "react";
@@ -14,7 +15,7 @@ import { UserContextProvider } from './UserContext';
 axios.defaults.baseURL = 'http://localhost:4000';
 // axios.defaults.withCredentials = true;
 function App() {
-  
+
   return (
     <UserContextProvider>
     <Routes>
