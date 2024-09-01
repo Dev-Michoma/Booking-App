a616648a6c793cf59cdac645ba94af744c8bc786
Dev <codesplint151@gmail.com>
2024-08-31 20:31:04 -0400

    these is the login Configuration

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 const User = require('./Models/User.js');
@@ -6,6 +7,7 @@ const  bcrypt = require('bcryptjs');
 require('dotenv').config()
 const app = express();
 const  bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "jdjvjndjvnhvksndvjnjv";
 
 app.use(express.json());
 app.use(cors({
@@ -24,7 +26,6 @@ app.get ('/test' ,(req,res)=>{
 
 app.post('/register' ,async (req,res) => {
    const {name ,email ,password} = req.body;
-
   try{
    const userDoc  =  await User.create({
       name,
@@ -35,8 +36,32 @@ app.post('/register' ,async (req,res) => {
   } catch (e){
    res.status(422).json(e);
   }
-  
    
+});
+
+
+
+app.post('/login', async (req, res) => {
+   try {
+       const { email, password } = req.body;
+       const userDoc = await User.findOne({ email });
+       if (userDoc) {
+           const passOk = bcrypt.compareSync(password, userDoc.password);
+           if (passOk) {
+            jwt.sign({email:userDoc.email , id:userDoc._id},jwtSecret ,{} ,(err,token)=>{
+               if(err) throw err;
+               res.cookie('token' ,'').json({ message: 'Login successful' });
+            });
+               
+           } else {
+               res.status(422).json({ error: 'Invalid credentials' });
+           }
+       } else {
+           res.status(404).json({ error: 'User not found' });
+       }
+   } catch (error) {
+       res.status(500).json({ error: 'An error occurred during login' });
+   }
 });
 
 app.listen(4000);
