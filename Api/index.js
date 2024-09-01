f79c3f5851593969572230a8dd2bbcf83190517b
Dev <codesplint151@gmail.com>
2024-08-31 21:03:11 -0400

    addition of routing to the dashboard

--- Api/index.js
+++ Api/index.js
@@ -12,7 +12,7 @@ const jwtSecret = "jdjvjndjvnhvksndvjnjv";
 app.use(express.json());
 app.use(cors({
    credentials: true,
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 
