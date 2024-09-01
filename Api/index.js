924bc0599939cd3fdb8cad65eaa4b16c8876a0b5
Dev <codesplint151@gmail.com>
2024-08-31 20:32:57 -0400

    tokenization of the app

--- Api/index.js
+++ Api/index.js
@@ -50,7 +50,7 @@ app.post('/login', async (req, res) => {
            if (passOk) {
             jwt.sign({email:userDoc.email , id:userDoc._id},jwtSecret ,{} ,(err,token)=>{
                if(err) throw err;
-               res.cookie('token' ,'').json({ message: 'Login successful' });
+               res.cookie('token' ,token).json({ message: 'Login successful' });
             });
                
            } else {
