92efd567a50b34cabbf0d3e6862b8dc74cf5a6e1
Dev Michoma <codesplint151@gmail.com>
2024-10-24 01:21:51 +0300

    fixes here and there

--- client/src/pages/Places.jsx
+++ client/src/pages/Places.jsx
@@ -60,15 +60,15 @@ function preInput(header ,description){
                         {preInput('Title' , 'Title for your place should be short and catchy')}
                      {/* <h2 className="text-2xl mt-4">Title</h2>
                      <p className="text-gray-500 text-sm">Title for your place should be short and catchy</p> */}
-                      <input type="text" placeholder="title ,for example: My Lovely apartment"/> 
+                      <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title ,for example: My Lovely apartment"/> 
                       {preInput('Address' , 'Title for your Address should be short and catchy')}
                       {/* <h2 className="text-xl mt-4">Address</h2>
                       <p className="text-gray-500 text-sm">Title for your Address should be short and catchy</p> */}
-                      <input type="text" placeholder="address"/> 
+                      <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="address"/> 
                       <h2>Photos</h2>
                       <p className="text-gray-500 text-sm"> More=Better </p>
                       <div className="flex gap-2">
-                        <input type = "text" placeholder={'Add using a Link ....jpg'}/>
+                        <input type = "text"   value={photoLink} onChange={ev => setPhotoLink(ev.target.value)} placeholder={'Add using a Link ....jpg'}/>
                         <button className="bg-gray-200 px-4 rounded-2xl grow">Add&nbsp;Photo</button>
                       </div>
 
@@ -89,15 +89,8 @@ function preInput(header ,description){
                      <h2 className="text-2xl mt-4">Perks</h2>
                      <p className="text-gray-500 text-sm"> Select all the parks from your place</p>
                      <div className="grid gap-3 mt-4 grid-cols-2 md:grid-cols-3">
-                        <Perks/>
-                        <label className="border gap-2 flex rounded-2xl p-4">
-                            <input type="checkbox"/>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                         </svg>
-
-                            <span>Entrance</span>
-                        </label>
+                        <Perks selected={perks} onChange={setPerks}/>
+                        
                      </div>
                      <h2 className="text-2xl mt-4">Extra Info</h2>
                      <p className="text-gray-500 text-sm"> These are the house Rules</p>
