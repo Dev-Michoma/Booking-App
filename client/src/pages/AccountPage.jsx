10805c8ea3ac29e697ad8bdfacb170e816827176
Dev Michoma <codesplint151@gmail.com>
2024-10-23 14:44:14 +0300

    working on the places page

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import {UserContext} from "../UserContext.jsx";
 import {Navigate ,Link ,useParams} from "react-router-dom";
 import axios from "axios";
+import PlacesPage from "./Places.jsx";
 
 
 export default function AccountPage(){
@@ -63,6 +64,13 @@ export default function AccountPage(){
               Logged in as {user.name}  ({user.email}) <br/>
              <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
 
+            </div>
+          )
+        }
+          {
+          subpage === 'places' && (
+            <div>
+              <PlacesPage/>
             </div>
           )
         }
