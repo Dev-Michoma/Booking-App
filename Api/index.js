2f9c70a17889b4c9399c41c8a4b51b48ad4ad26c
Dev Michoma <codesplint151@gmail.com>
2024-10-25 16:56:07 +0300

    Addition of state to the perks

--- Api/index.js
+++ Api/index.js
@@ -111,16 +111,16 @@ app.post('/upload-by-link' ,async (req,res)=>{
 
 const photosMiddleware = multer ({dest: 'uploads'});
 app.post('/upload',photosMiddleware.array('photos' ,100 ), (req,res) =>{
-    const uploadedFiles =[];
-    for (let i= 0; i < req.files.length ;i++){
+
+    const uploadedFiles =[]; //An empty array is initialized  to store the path of renamed files
+    for (let i= 0; i < req.files.length ;i++){ ///A loop to iterate through  the uploaded files
         const {path ,originalname} = req.files[i];
-        const parts = originalname.split('.');
+        const parts = originalname.split('.'); //orifginal name is split to extract the file extension
         const ext = parts[parts.length -1 ];
         const newPath = path + '.' + ext;
-         fs.renameSync(path , newPath);
+         fs.renameSync(path , newPath); //function is used to rename the file from its temporary path to the new path
          uploadedFiles.push(newPath.replace('uploads/',''));
     }
-
 
   res.json(uploadedFiles);
 });
