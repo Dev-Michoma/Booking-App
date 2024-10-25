2b0df92ef1226fd1b91a59fe1228e65082cc2be8
Dev Michoma <codesplint151@gmail.com>
2024-10-25 16:58:48 +0300

    Minor hot fix

--- client/src/pages/Perks.jsx
+++ client/src/pages/Perks.jsx
@@ -10,7 +10,7 @@ export default function Perks ({selected ,onChange}){
     if(checked){
         onChange([...selected ,name]);
     }else {
-        onChange([...selected.filtre(selectedName => selectedName !== name)]);
+        onChange([...selected.filter(selectedName => selectedName !== name)]);
     }
 
     }
