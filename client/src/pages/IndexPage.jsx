7d159ba2f3bb4bf9e9eb469e6ab261d87ff1cd8b
Dev Michoma <codesplint151@gmail.com>
2024-11-02 07:56:54 +0300

    Just a few iterations

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -6,20 +6,27 @@ export  default function IndexPage(){
       const [landingpages,setLandingpages] = useState([]);
      useEffect (()=>{
           axios.get('/landingpages').then(response =>{
-          setLandingpages(response.data);
+          setLandingpages([...response.data,...response.data ,...response.data ,...response.data]);
           });
      },[]);
     return (
-        <div>
+        <div className="grid grid-cols-4 gap-4 md:grid-cols-3 lg:grid-cols-6 mt-8 " >
           {
             landingpages.length > 0 && landingpages.map(landingpage => (
                 <div key={landingpage.id}>
                     
+                    <div className="bg-gray-500 rounded-2xl flex">
                     {landingpage.photos?.[0] &&(
-                        <img src={'http://localhost:4000/uploads/'+landingpage.photos?.[0]} alt=""/>
+                        <div>
+                        <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/'+landingpage.photos?.[0]} alt=""/>
+                        </div>
                     )}
+                    </div>
+                    <div className="mt-2">
+
+                        <h2 className="text-sm">{landingpage.title}</h2>
                     
-                    {landingpage.title}
+                    </div>
                 </div>
             ))
           }
