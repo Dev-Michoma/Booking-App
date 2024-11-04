4aaea440aba4b26a079aed2c0358a7e6744817bb
Dev Michoma <codesplint151@gmail.com>
2024-11-05 00:53:22 +0300

    useState to show Photos

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 export default function PlacePage(){
      const {id} = useParams();
      const [place ,setPlace] = useState(null);
+     const [showAllPhotos ,setShowAllPhotos] = useState(false);
 
      useEffect (() => {
 
@@ -19,6 +20,13 @@ export default function PlacePage(){
 
      if(!place) return '';
 
+     if(showAllPhotos){
+     return(
+     <div className=""> 
+     show photos
+     </div>);
+     }
+ //Research between the difference of Relative and absolute
     return(
       <div className="mt-4 bg-gray-100 -mx-4 px-8 py-2">
         {/* place page: {id} */}
@@ -64,7 +72,13 @@ export default function PlacePage(){
                         </div>
             </div>
           </div>
-            <button className="absolute bottom-o right-0">absolute</button>
+            <button onClick={() => setShowAllPhotos(true)}className=" flex gap-1 absolute bottom-2 bg-2 rounded-2xl shadow-md shadow-gray-500 right-2 py-2 px-4">
+                
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+             </svg>
+
+                Show more Photos</button>
           </div>
       </div>
 
