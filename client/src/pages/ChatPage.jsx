90add3cbdf1d603663e44760cbad57c031c82b47
Dev Michoma <codesplint151@gmail.com>
2024-11-22 05:37:53 +0300

    Styling the ChatPage to fulfill the custom needs

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -8,7 +8,25 @@ export default function ChatPage(){
     return (
      <div>
     <AccountNav/>
-    <p>Michoma </p>
+      
+      <div className="flex h-screen border-2 h-[75vh] border-gray-300 mx-64">
+      <div className="bg-blue-100 w-1/3">Contacts</div>
+      <div className="bg-blue-300    flex  flex-col w-2/3">
+      <div className="flex-grow mt-4 mx-6">Messages With Selected Person</div>
+
+
+      <div className="flex  bottom-28 left-0 w-full p-4 mx-2 "> 
+       <input type="text" className="bg-white border bottom-0 p-2 w-4 " placeholder="Type Your Message Here"/>
+         <button className="mx-4 bg-blue-300">
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+         </button>
+         
+      </div>
+
+      </div>
+      </div>
      </div>
       
     )
