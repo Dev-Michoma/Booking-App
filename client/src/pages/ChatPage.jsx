bfaf36c9ab26fe99ed45691c5f971bf48513bc19
Dev Michoma <codesplint151@gmail.com>
2024-11-26 11:03:04 +0300

    styling the Frontend

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -20,7 +20,7 @@ export default function ChatPage(){
      <div>
     <AccountNav/>
       
-      <div className="flex h-screen border-2 rounded-lg h-[75vh] border-gray-300 mx-64">
+      <div className="flex h-screen border-2 rounded-lg h-[65vh] border-gray-300 mx-64">
       <div className="bg-blue-100 w-1/3">Contacts</div>
       <div className="bg-blue-300    flex  flex-col w-2/3">
       <div className="flex-grow mt-4 mx-6">Messages With Selected Person</div>
